53c74977bdf08f33756045dae355d92ece138e88
manav jain <mjmanav4@gmail.com>
2019-08-31 11:50:34 +0530

    Add files via upload

---
+++ HappyFortuneService.java
@@ -0,0 +1,14 @@
+package com.manavjain.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HappyFortuneService implements FortuneService {
+
+	@Override
+	public String getFortune() {
+		// TODO Auto-generated method stub
+		return "Happy fortune service";
+	}
+
+}
---
+++ TennisCoach.java
@@ -0,0 +1,28 @@
+package com.manavjain.springdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+
+	
+	private FortuneService fortuneService;
+	
+	@Autowired
+	public TennisCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+	
+	@Override
+	public String getDailyWorkout() {
+		return "Practice your smash";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return fortuneService.getFortune();
+	}
+
+}
