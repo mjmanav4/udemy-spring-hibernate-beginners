d52f6b40e0303a968c264cbc4d9eeaa030d5c6fb
manav jain <mjmanav4@gmail.com>
2019-09-02 23:50:17 +0530

    Add files via upload

---
+++ annotation/method/TennisCoach.java
@@ -0,0 +1,40 @@
+package com.manavjain.springdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+
+	
+	private FortuneService fortuneService;
+	
+	
+	public TennisCoach() {
+		System.out.println("constructor tennis coach ");
+	}
+	/*@Autowired
+	public TennisCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+	*/
+	
+	@Autowired
+	public void defaultmethod(FortuneService theFortuneService) {
+		System.out.println("default method tennis coach ");
+	
+		fortuneService = theFortuneService;
+		
+	}
+	@Override
+	public String getDailyWorkout() {
+		return "Practice your smash";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return fortuneService.getFortune();
+	}
+
+}
