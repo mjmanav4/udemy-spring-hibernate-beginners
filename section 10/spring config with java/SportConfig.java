398b14ae1cbfdb92292895c591bc93814d52dfbc
manav jain <mjmanav4@gmail.com>
2019-09-12 10:18:23 +0530

    Add files via upload

---
+++ section 10/spring config with java/SportConfig.java
@@ -0,0 +1,10 @@
+package com.manavjain.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.manavjain.springdemo")
+public class SportConfig {
+
+}
